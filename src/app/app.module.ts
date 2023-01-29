4a8b42ba191d5878f67e15da7a4c7e98d8240b36
tinaandriamarosoa <tina.andriamarosoa@solar-transit.net>
2023-01-30 02:28:02 +0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,8 @@ import { ActiveModule } from 'projects/active/src/src';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentModule } from './component/component.module';
-import { DirectiveModule } from './directive/directive.module';
 import { ClientModule } from './page/client/client.module';
 import { ResponsableModule } from './page/responsable/responsable.module';
-import { PageModule } from './page/page.module';
-import { PipeModule } from './pipe/pipe.module';
 import { SharedModule } from './shared/shared.module';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
