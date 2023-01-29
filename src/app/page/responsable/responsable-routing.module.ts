4b22fa7248dce7987758c3b0d9994623fff02c35
TsikyNyAina <tramasimiarantsoa@gmail.com>
2023-01-29 17:07:37 +0300

    piece marque
    modif

--- src/app/page/responsable/responsable-routing.module.ts
+++ src/app/page/responsable/responsable-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MarqueVoitureSaveComponent } from '../marque-voiture-save/marque-voiture-save.component';
 import { MarqueVoitureComponent } from '../marque-voiture/marque-voiture.component';
 import { ResponsableComponent } from './responsable.component'; 
+import { MarquePieceComponent } from '../marque-piece/marque-piece.component';
 
 const routes: Routes = [
   { path:"", component:ResponsableComponent },
   { path:"marqueVoiture/save", component:MarqueVoitureSaveComponent },
-  { path:"marqueVoiture", component:MarqueVoitureComponent }
+  { path:"marqueVoiture", component:MarqueVoitureComponent },
+  { path:"marquePiece", component:MarquePieceComponent }
 ];
 
 @NgModule({
