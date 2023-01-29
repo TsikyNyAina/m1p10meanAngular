5b2d02f25132d7715d16e76524e83e69b4feb8dc
TsikyNyAina <tramasimiarantsoa@gmail.com>
2023-01-29 14:54:18 +0300

    voiture
    select modele

--- src/app/page/client/voiture/voiture.component.ts
+++ src/app/page/client/voiture/voiture.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Voiture } from 'src/app/entity/Voiture';
 import { ClientComponent } from '../client.component';
 import { ClientService } from '../client.service';
+import { getItem } from 'projects/active/src/lib/utils';
 
 @Component({
   selector: 'app-voiture',
@@ -14,7 +15,11 @@ export class VoitureComponent implements OnInit {
 
   constructor(private clientComponent:ClientComponent,private clientService:ClientService) {  
     this.clientService.getVoitureByClientId(this.clientComponent.clientId).subscribe((voiture:any)=>console.dir(this.voiture=voiture))
- 
+    this.clientService.getModeleVoiture().subscribe((modele:any)=>{
+      console.log(modele);
+      
+      getItem(this.voiturei).modeleId.data=modele 
+    });
     
   } 
 
